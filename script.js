0b53d002cd4ac5bdcafa8fa1ca922eefc65345d7
André Ciappina <32151463+dehciappina@users.noreply.github.com>
2018-08-13 21:36:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,30 +17,87 @@
 // SEE PROFILE BUTTON
 
 let see_profile = document.querySelector('.see_more')
+let name_bar = document.querySelector('.name_bar')
+let profile_photo = document.querySelector('.image_container')
 let profile_window = document.querySelector('.hidden_profile')
 let voltar_playmatch = document.querySelector('.return')
 
-see_profile.addEventListener('click', function() {
-    profile_window.classList.add('show_hidden_profile')
-})
+see_profile.addEventListener('click', showHiddenProfile)
+name_bar.addEventListener('click', showHiddenProfile)
+profile_photo.addEventListener('click', showHiddenProfile)
 
 voltar_playmatch.addEventListener('click', function() {
     profile_window.classList.remove('show_hidden_profile')
 })
 
 
+function showHiddenProfile() {
+    profile_window.classList.add('show_hidden_profile')
+}
 
 
-var windowWidth = document.body.clientWidth;
-
-    console.log('aiaiai')
 
 
 
-// SUBWINDOW CHANGE
-var friendsIcon = document.querySelector('#friends')
-var msgIcon = document.querySelector('#messages')
-var combinationsIcon = document.querySelector('#combinations')
+
+
+
+
+// LIKE E UNLIKE
+var likeButton = document.querySelector('.like_container')
+var unlikeButton = document.querySelector('.unlike_container')
+
+likeButton.addEventListener('click', function() {
+    if(!this.querySelector('img').classList.contains('like_container_move')) {
+        this.querySelector('img').classList.add('like_container_move');
+    } else {
+        this.querySelector('img').classList.remove('like_container_move');
+    }
+})
+
+unlikeButton.addEventListener('click', function() {
+
+    if(!this.querySelector('img').classList.contains('unlike_container_move')) {
+        this.querySelector('img').classList.add('unlike_container_move');
+    } else {
+        this.querySelector('img').classList.remove('unlike_container_move');
+    }
+})
+
+
+
+
+
+
+
+// UNLIKE/LIKE CLICK ANIMATIONS
+
+let playMiddle = document.querySelector('.playmatch_middle')
+let compatibility = document.querySelector('.compatibility')
+let compatibilityLevel = document.querySelector('.level_container')
+let compatibilityH2 = document.querySelector('.compatibility h2')
+
+function unlikePressed() {
+
+    if(!playMiddle.classList.contains('playmatch_middle_unlike_animation')) {
+        playMiddle.classList.add('playmatch_middle_unlike_animation')
+        compatibility.classList.add('compatibility_unlike_animation')
+        compatibilityLevel.classList.add('level_container_unlike_animation')
+        compatibilityH2.classList.add('compatibility_h2_unlike_animation')
+    } else {
+        playMiddle.classList.remove('playmatch_middle_unlike_animation')
+        compatibility.classList.remove('compatibility_unlike_animation')
+        compatibilityLevel.classList.remove('level_container_unlike_animation')
+        compatibilityH2.classList.remove('compatibility_h2_unlike_animation')
+    }
+
+}
+
+unlikeButton.addEventListener('click', unlikePressed)
+
+
+
+
 
 // BOTÕES SUPERIORES
 var profileButton = document.querySelector('#PROFILE')
@@ -162,26 +219,6 @@ topButtons[3].addEventListener("click", topButtonClick);
 
 
 
-// LIKE E UNLIKE
-var likeButton = document.querySelector('.like_container')
-var unlikeButton = document.querySelector('.unlike_container')
-
-likeButton.addEventListener('click', function() {
-    if(!this.querySelector('img').classList.contains('like_container_move')) {
-        this.querySelector('img').classList.add('like_container_move');
-    } else {
-        this.querySelector('img').classList.remove('like_container_move');
-    }
-})
-
-unlikeButton.addEventListener('click', function() {
-
-    if(!this.querySelector('img').classList.contains('unlike_container_move')) {
-        this.querySelector('img').classList.add('unlike_container_move');
-    } else {
-        this.querySelector('img').classList.remove('unlike_container_move');
-    }
-})
 
 
 
@@ -194,42 +231,6 @@ unlikeButton.addEventListener('click', function() {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-// UNLIKE/LIKE CLICK ANIMATIONS
-
-let playMiddle = document.querySelector('.playmatch_middle')
-let compatibility = document.querySelector('.compatibility')
-let compatibilityLevel = document.querySelector('.level_container')
-let compatibilityH2 = document.querySelector('.compatibility h2')
-
-function unlikePressed() {
-
-    if(!playMiddle.classList.contains('playmatch_middle_unlike_animation')) {
-        playMiddle.classList.add('playmatch_middle_unlike_animation')
-        compatibility.classList.add('compatibility_unlike_animation')
-        compatibilityLevel.classList.add('level_container_unlike_animation')
-        compatibilityH2.classList.add('compatibility_h2_unlike_animation')
-    } else {
-        playMiddle.classList.remove('playmatch_middle_unlike_animation')
-        compatibility.classList.remove('compatibility_unlike_animation')
-        compatibilityLevel.classList.remove('level_container_unlike_animation')
-        compatibilityH2.classList.remove('compatibility_h2_unlike_animation')
-    }
-
-}
-
-unlikeButton.addEventListener('click', unlikePressed)
 
 
 
